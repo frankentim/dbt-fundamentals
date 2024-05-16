3d20957f7d7b1a585b2406f7da85b9209e9e805f
Tim Franken <tim.franken@biztory.com>
2024-05-16 12:20:59 +0000

    Final steps

--- analysis/compare_queries.sql
+++ analysis/compare_queries.sql
@@ -1,11 +1,27 @@
-{% set old_etl_relation=ref('customer_orders') %} 
+-- {% set old_etl_relation=ref('customer_orders') %} 
 
-{% set dbt_relation=ref('fct_customer_orders') %}  
+-- {% set dbt_relation=ref('fct_customer_orders') %}  
 
-{{ 
+-- {{ 
 
-audit_helper.compare_relations(
-        a_relation=old_etl_relation,
-        b_relation=dbt_relation,
-        primary_key="order_id"
-    ) }}
+-- audit_helper.compare_relations(
+--         a_relation=old_etl_relation,
+--         b_relation=dbt_relation,
+--         primary_key="order_id"
+--     ) }}
+
+{# in dbt Develop #}
+
+{% set old_etl_relation=adapter.get_relation(
+      database="SANDBOX_TIMFRANKEN",
+      schema="dbt_tfranken",
+      identifier="customer_orders"
+) -%}
+
+{% set dbt_relation=ref('fct_customer_orders') %}
+
+{{ audit_helper.compare_relations(
+    a_relation=old_etl_relation,
+    b_relation=dbt_relation,
+    primary_key="order_id"
+) }}
